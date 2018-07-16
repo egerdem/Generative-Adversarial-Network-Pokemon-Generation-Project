f82c936d43dbbd4414d21bb8a4e7e43a35b64f33
Ege Erdem <34795370+egerdem@users.noreply.github.com>
2018-07-16 23:16:45 +0300

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,14 +1,23 @@
 # resize pokeGAN.py
 import os
 import cv2
+from PIL import Image
 
 src = "./data" #pokeRGB_black
-dst = "./resizedData" # resized
+dst = "./resizeddate_ege" # resized
 
 os.mkdir(dst)
 
 for each in os.listdir(src):
-    img = cv2.imread(os.path.join(src,each))
-    img = cv2.resize(img,(256,256))
-    cv2.imwrite(os.path.join(dst,each), img)
+    imga = Image.open(os.path.join(src,each))
+    new = imga.resize(img,(256,256))
+    new.save(os.path.join(dst,each))
+dst = "./resized_black_ege/"
+os.mkdir(dst)
+
+for each in os.listdir(src)
+    png = Image.open(os.path.join(src,each))
+    #print each
+    
+    Image.open(os.path.join(src,each)).convert('RGB').save(os.path.join(dst,each.split('.')[0]+ '.jpg'), 'JPEG')
     
